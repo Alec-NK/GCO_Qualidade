86419b0c60708a2722df4a052c4b21c11cd7f739
Alec-NK <55238706+Alec-NK@users.noreply.github.com>
2020-10-27 23:26:37 -0300

    Add Javascript com Jquery

---
+++ js/funcoes.js
@@ -0,0 +1,3 @@
+$(new Document).ready(function(){
+
+});
---
+++ js/salvarCampos.js
@@ -0,0 +1,14 @@
+$(new Document).ready(function(){
+
+});
+
+function salvarXML(){
+
+    var naoConf = $("naoConf").val();
+    var acaoCor = $("actCor").val();
+    var prazo = $("prazo").val();
+    var entrega = $("entrega").val();
+    var check = $("check").val();
+
+    
+}
